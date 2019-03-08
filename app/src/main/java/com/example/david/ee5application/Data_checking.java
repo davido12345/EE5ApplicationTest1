e3dad8cba348a6391d464dcffa7bae74ad15f257
qingyuan.cao <cqy214gitlab>
2019-03-08 10:26:44 +0100

    update2

--- app/src/main/java/com/example/david/ee5application/Data_checking.java
+++ app/src/main/java/com/example/david/ee5application/Data_checking.java
@@ -1,8 +1,11 @@
 package com.example.david.ee5application;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 public class Data_checking extends AppCompatActivity {
@@ -12,11 +15,28 @@ public class Data_checking extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.data_checking);
 
-        String[] ctype = new String[]{"Brussel","Gent","Anterwep"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,ctype);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this, R.array.choose, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-        Spinner spinner = super.findViewById(R.id.Session_Spinner);
+        final Spinner spinner = super.findViewById(R.id.Session_Spinner);
+        Button OK = (Button) findViewById(R.id.OK_button);
+
         spinner.setAdapter(adapter);
+        OK.setOnClickListener(new Button.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent();
+                switch (spinner.getSelectedItemPosition()) {
+                    case 0:
+                        intent.setClass(Data_checking.this, Data_page.class);
+                        startActivity(intent);
+                        break;
+                }
+            }
+        });
+
     }
 }
